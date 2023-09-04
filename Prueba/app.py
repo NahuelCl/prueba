48510fb6da49ff0f301bba7681aa69183805adfe
Nahuel Clauser <clausernahuel@gmail.com>
2023-09-04 00:53:27 -0300

    Probando

--- Prueba/app.py
+++ Prueba/app.py
@@ -1,19 +1,19 @@
 from flask import Flask, request, jsonify, render_template
-import joblib  # Import joblib to load your pre-trained model
+import joblib  
 import numpy as np
 
 app = Flask(__name__)
 
-# Load your pre-trained classification model here
-model = joblib.load('svc_trained.pkl')  # Replace with the path to your model file
+
+model = joblib.load('svc_trained.pkl') 
 
 @app.route('/')
 def index():
-    return render_template('index.html')  # Render the HTML form
+    return render_template('index.html')  
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    # Extract data from the form
+
     Glucose = float(request.form['Glucose'])
     BloodPressure = float(request.form['BloodPressure'])
     Insulin = float(request.form['Insulin'])
@@ -21,20 +21,20 @@ def predict():
     DiabetesPedigreeFunction = float(request.form['DiabetesPedigreeFunction'])
     Age = float(request.form['Age'])
 
-    # Prepare the input data for classification
+   
     input_data = np.array([[ Glucose, BloodPressure,Insulin, BMI, DiabetesPedigreeFunction,Age]])
 
-    # Perform classification using the pre-trained model
-    predicted_class = model.predict(input_data)[0]  # Get the prediction (0 or 1)
+   
+    predicted_class = model.predict(input_data)[0]  
 
-    # Define the result message based on the prediction
+   
     if predicted_class == 1:
-        result_message = "The person is classified as having diabetes."
+        result_message = "La persona ha sido clasificada como que tiene diabetes."
     else:
-        result_message = "The person is classified as not having diabetes."
+        result_message = "La persona ha sido clasificada como que no tiene diabetes."
 
-    # Return the classification result as JSON
-    return jsonify({'result': result_message})
+ 
+    return jsonify({'Clasificacion': result_message})
 
 if __name__ == '__main__':
     app.run(debug=True)
